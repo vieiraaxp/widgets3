98eac0f22ef988bc4a19ab79ea4cb9c233e1527d
vieiraaxp <matheusaquila.vieira@gmail.com>
2024-04-26 15:23:44 -0300

    exercicios em dupla com José

--- chackbox.py
+++
@@ -1,9 +0,0 @@
-from kivy.app import App
-from kivy.uix.checkbox import CheckBox
-
-class CheckBoxApp(App):
-    def build(self):
-        return CheckBox(active=False)
-
-if __name__ == '__main__':
-    CheckBoxApp().run()
--- image.py
+++
@@ -1,9 +0,0 @@
-from kivy.app import App 
-from kivy.uix.image import Image
-
-class MyApp(App):
-    def build(self):
-        return Image(source='/Users/aluno.sesipaulista/Downloads/vieiraa.jpeg')
-
-if __name__ == '__main__':
-    MyApp().run()
